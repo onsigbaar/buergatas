6ea375f3051c9c1febe6c5d8b2ff31c1e3f149b3
anonymoussc <50c5ac69@opayq.com>
2023-07-10 12:28:09 +0700

    feat(main): modified kubernetes
    -
    
    BREAKING CHANGE: -
    
    Closes -

--- spring-cloud/gateway/src/main/java/sc/app/springcloud/gateway/SecurityConfig.java
+++ spring-cloud/gateway/src/main/java/sc/app/springcloud/gateway/SecurityConfig.java
@@ -25,7 +25,6 @@ public class SecurityConfig {
         .pathMatchers("/error/**").permitAll()
         .pathMatchers("/openapi/**").permitAll()
         .pathMatchers("/webjars/**").permitAll()
-        .pathMatchers("/config/**").permitAll()
         .anyExchange().authenticated()
         .and()
       .oauth2ResourceServer()
